6dfcf02fcd996eb8c78018d5049501aad4c222cc
Funtastix <funtastix@github.com>
2023-02-07 23:05:05 +1100

    fix small bugs collected in issue #45 and bump version to 0.10.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh: # pylint: disable=unspecified-encoding
 setup(
     name="pyrinnaitouch",
     packages=find_packages(exclude=["tests", "tests.*"]),
-    version="0.10.3",
+    version="0.10.4",
     license="mit",
     description="A python interface to the Rinnai Touch Wifi controller",
     long_description=LONG_DESCRIPTION,
